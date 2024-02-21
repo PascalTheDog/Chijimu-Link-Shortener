a1518f23d1379e364cd9bb698e971f05faace6f2
David L <davidl.139573@gmail.com>
2024-02-21 22:17:39 +0000

    Add project files.

---
+++ Chijimu.Common/Interfaces/IConvertService.cs
@@ -0,0 +1,7 @@
+﻿namespace Chijimu.Core.Services.Interfaces;
+
+public interface IConvertService
+{
+    public string GetFullURL(string shortenedUrl);
+    public string ShortenURL(string url);
+}
---
+++ Chijimu.Core/Services/ConvertService.cs
@@ -0,0 +1,38 @@
+﻿using Chijimu.Core.Services.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Chijimu.Core.Services;
+
+public class ConvertService : IConvertService
+{
+    private readonly ILogger<ConvertService> _logger;
+
+    public ConvertService(ILogger<ConvertService> logger)
+    {
+        _logger = logger;
+    }
+
+    public string GetFullURL(string shortenedUrl)
+    {
+        _logger.LogTrace("[{method}({param})]", nameof(GetFullURL), shortenedUrl);
+
+        if (shortenedUrl == null)
+        {
+            throw new ArgumentNullException(nameof(shortenedUrl));
+        }
+
+        return "success";
+    }
+
+    public string ShortenURL(string url)
+    {
+        _logger.LogTrace("[{method}({param})]", nameof(ShortenURL), url);
+
+        if (string.IsNullOrEmpty(url))
+        {
+            throw new ArgumentNullException(nameof(url));
+        }
+
+        return "success";
+    }
+}
